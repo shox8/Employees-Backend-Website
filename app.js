318bfbfc52f1905f5694198baf832fe4dcbd226c
Shoxruh <codershoxruh@gmail.com>
2024-03-25 10:38:18 +0500

    Updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors");
 
 require("dotenv").config()
 
@@ -10,6 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/user', require('./routes/user'));
 app.use('/api/employees', require('./routes/employees'));
